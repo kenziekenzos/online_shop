c509211779e8544d273bed50e54ca0e3dae710c2
Kenza <kenza.mezouar@gmail.com>
2019-09-02 19:20:52 +0200

    [CREATE] User Controller View Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+  sessions: 'users/sessions'
+ }
   resources :items
+  resources :users
+
   get 'item/index' => 'item#index'
   root to: 'items#index'
+  #get 'users/new' => 'users#new'
+get 'users/sign_in' => 'users/sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
