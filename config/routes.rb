7b52e0b66fca0821a28c78b1dc0380c5c7b9508f
Kenza <kenza.mezouar@gmail.com>
2019-09-02 16:42:01 +0200

    [CREATE] Views Item index & show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :items
   get 'item/index' => 'item#index'
-
+  root to: 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
